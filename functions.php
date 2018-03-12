2453a93152d873a6454fe2aed16b3181d2fe81e0
nedrahin <parvezzpreo@gmail.com>
2018-03-12 22:40:19 +0600

    2 extra css file add

--- functions.php
+++ functions.php
@@ -1,26 +1,24 @@
 <?php
 	
-	if ( ! function_exists('theme_init')) 
-	{
+	if ( ! function_exists('theme_init')) :
 		function theme_init()
 		{
 			add_theme_support( 'title-tag' );
 			
 		}
-		add_action( 'init', 'theme_init' );
-		
-	}
+		add_action( 'init', 'theme_init' );		
+	endif;
 
-	if ( ! function_exists('theme_enqueue')) 
-	{
+	if ( ! function_exists('theme_enqueue')) :
 		function theme_enqueue()
 		{
-			wp_enqueue_style( 'bootstrap-min-css', get_template_directory_uri().'/css/bootstrap.min.css', 'null', 'v4.0.0-beta', 'all' );
+			wp_enqueue_style( 'bootstrap-min', get_template_directory_uri().'/css/bootstrap.min.css', 'null', 'v4.0.0-beta', 'all' );
+			wp_enqueue_style( 'main', get_template_directory_uri().'/css/main.css', 'null', 'v1.0', 'all' );
+			wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 			wp_enqueue_script('jquery-min-js', get_template_directory_uri().'/js/jquery.min.js', 'null', 'v3.3.1', true);
 			wp_enqueue_script('popper-min-js', get_template_directory_uri().'/js/popper.min.js', 'null', 'Federico Zivolo 2017', true);
 			wp_enqueue_script('bootstrap-min-js', get_template_directory_uri().'/js/bootstrap.min.js', 'null', 'v4.0.0-beta', true);
 		}
 		add_action( 'wp_enqueue_scripts', 'theme_enqueue' );
-
-	}
+	endif;
--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<!-- Required meta tags -->
-		<meta charset="utf-8">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!--[if lt IE 9]>
+	    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	    <![endif]-->
     	<?php wp_head(); ?>
 	</head>
-	<body>
+	<body <?php body_class(); ?>>
 		<h1>Hello, world!</h1>
 
