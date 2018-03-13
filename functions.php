6b76a064501bf0ff53dc34f770ae305919905d82
nedrahin <parvezzpreo@gmail.com>
2018-03-13 12:20:38 +0600

    Slider Add will be more update

--- functions.php
+++ functions.php
@@ -12,15 +12,27 @@
 	if ( ! function_exists('theme_enqueue')) :
 		function theme_enqueue()
 		{
+			wp_enqueue_style( 'fonts-min', get_template_directory_uri().'/fonts/feather/style.css', 'null', 'v4.0.0-beta', 'all' );
+			
 			wp_enqueue_style( 'bootstrap-min', get_template_directory_uri().'/css/bootstrap.min.css', 'null', 'v4.0.0-beta', 'all' );
 			wp_enqueue_style( 'main', get_template_directory_uri().'/css/main.css', 'null', 'v1.0', 'all' );
-			wp_enqueue_style( 'sliderman', get_template_directory_uri().'/css/sliderman.css', 'null', 'v1.0', 'all' );
+
+			wp_enqueue_style( 'demo', get_template_directory_uri().'/zoms/demo.css', 'null', 'v1.0', 'all' );
+			wp_enqueue_style( 'component', get_template_directory_uri().'/zoms/component.css', 'null', 'v1.0', 'all' );
 			wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+
 
 			// wp_enqueue_script('jquery-min-js', get_template_directory_uri().'/js/jquery.min.js', 'null', 'v3.3.1', true);
 			wp_enqueue_script('jquery');
 			wp_enqueue_script('popper-min-js', get_template_directory_uri().'/js/popper.min.js', 'null', 'Federico Zivolo 2017', true);
 			wp_enqueue_script('bootstrap-min-js', get_template_directory_uri().'/js/bootstrap.min.js', 'null', 'v4.0.0-beta', true);
+
+			// zoom slider js
+			wp_enqueue_script('modernizr-min-js', get_template_directory_uri().'/zoom/modernizr.custom.js', 'null', 'v4.0', true);
+			wp_enqueue_script('classie-min-js', get_template_directory_uri().'/zoom/classie.js', 'null', 'v4.0', true);
+			wp_enqueue_script('dynamics-min-js', get_template_directory_uri().'/zoom/dynamics.min.js', 'null', 'v4.0', true);
+			wp_enqueue_script('main-min-js', get_template_directory_uri().'/zoom/main.js', 'null', 'v4.0', true);
 		}
 		add_action( 'wp_enqueue_scripts', 'theme_enqueue' );
 	endif;
---
+++ slioder-header.php
@@ -0,0 +1,10 @@
+	<header class="bp-header cf">
+		<span>Blueprint <span class="bp-icon bp-icon-about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
+		<h1>Zoom Slider</h1>
+		<nav>
+			<a href="http://tympanus.net/Blueprints/FilterableProductGrid/" class="bp-icon bp-icon-prev" data-info="previous Blueprint"><span>Previous Blueprint</span></a>
+			<!--a href="" class="bp-icon bp-icon-next" data-info="next Blueprint"><span>Next Blueprint</span></a-->
+			<a href="http://tympanus.net/codrops/?p=24330" class="bp-icon bp-icon-drop" data-info="back to the Codrops article"><span>back to the Codrops article</span></a>
+			<a href="http://tympanus.net/codrops/category/blueprints/" class="bp-icon bp-icon-archive" data-info="Blueprints archive"><span>Go to the archive</span></a>
+		</nav>
+	</header>
